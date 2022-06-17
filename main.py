620aa3db6f28e151848489b5814d5f0a0b5b2cd8
tophermckee@gmail.com <tophermckee@gmail.com>
2022-06-17 12:46:17 -0500

    append logs

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s',
     datefmt='%B-%d-%Y %H:%M:%S',
     filename=f"./logs/{Path(__file__).stem}.log",
-    filemode='w'
+    filemode='a'
 )
 
 pp = pprint.PrettyPrinter(indent=2)
