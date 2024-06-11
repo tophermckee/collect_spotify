803ddb170759a4bc16bf24299e9db28e6056e494
Topher McKee <tophermckee@gmail.com>
2024-06-11 14:19:01 -0500

    log scripts

---
+++ logs/end_spotify_cron.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+LOG_FILE="$HOME/collect_spotify/logs/cron_logs.txt"
+START_TIME=$(date +"%I:%M %p")
+DATE=$(date +"%Y-%m-%d")
+
+echo "download spotify cron finished at $START_TIME on $DATE" >> $LOG_FILE
